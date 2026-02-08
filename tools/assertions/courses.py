cfaec940552ac1f90685e97d727b5681744d3fc7
Wildips <zak125@mail.ru>
2026-02-08 15:15:33 +0300

    Практикуемся в написании API автотестов. Часть 2. Шаг 10.

--- tools/assertions/courses.py
+++ tools/assertions/courses.py
@@ -1,5 +1,5 @@
 from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, CourseSchema, \
-    GetCoursesResponseSchema, CreateCourseResponseSchema
+    GetCoursesResponseSchema, CreateCourseResponseSchema, CreateCourseRequestSchema
 from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.files import assert_file
 from tools.assertions.users import assert_user
@@ -67,3 +67,21 @@ def assert_get_courses_response(
 
     for index, create_course_response in enumerate(create_course_responses):
         assert_course(get_courses_response.courses[index], create_course_response.course)
+
+
+def assert_create_course_response(request: CreateCourseRequestSchema, response: CreateCourseResponseSchema):
+    """
+    Проверяет, что данные создания курса в ответе соответствуют данным из запроса создания
+    :param request: исходные данные
+    :param response: получаемые данные
+    :raises AssertionError: Если данные курса не совпадают
+    """
+
+    assert_equal(request.title, response.course.title, "title")
+    assert_equal(request.max_score, response.course.max_score, "max_score")
+    assert_equal(request.min_score, response.course.min_score, "min_score")
+    assert_equal(request.description, response.course.description, "description")
+    assert_equal(request.estimated_time, response.course.estimated_time, "estimated_time")
+
+    assert_equal(request.preview_file_id, response.course.preview_file.id, "preview_file_id")
+    assert_equal(request.created_by_user_id, response.course.created_by_user.id, "created_by_user_id")
