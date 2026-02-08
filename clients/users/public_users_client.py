cac7a366673f941368ae57c1bbd6f087dccabf22
Wildips <zak125@mail.ru>
2026-02-08 22:10:23 +0300

    Добавление Allure шагов в автотесты. Шаг 7.

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -1,3 +1,5 @@
+import allure
+
 from httpx import Response
 
 from clients.api_client import APIClient
@@ -10,6 +12,7 @@ class PublicUsersClient(APIClient):
     Клиент для работы с /api/v1/users
     """
 
+    @allure.step("Create user")
     def create_user_api(self, request: CreateUserRequestSchema) -> Response:
         """
         Метод создает пользователя.
