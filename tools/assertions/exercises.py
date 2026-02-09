29feed57aa892976621c4718edd775d48f86b4e1
Wildips <zak125@mail.ru>
2026-02-10 00:33:47 +0300

    Добавляем логирование для API автотестов. Шаг 13.

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -6,6 +6,9 @@ from clients.exercises.exercises_schema import CreateExerciseRequestSchema, Crea
     GetExercisesResponseSchema
 from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.errors import assert_internal_error_response
+from tools.logger import get_logger
+
+logger = get_logger("EXERCISES_ASSERTIONS")
 
 
 @allure.step("Check create exercise response")
@@ -17,6 +20,7 @@ def assert_create_exercise_response(response: CreateExerciseResponseSchema, requ
     :param response: ответ на создание задания
     :raises AssertionError: если хоть одно поле не совпадает
     """
+    logger.info("Check create exercise response")
 
     assert_equal(response.exercise.title, request.title, "title")
     assert_equal(response.exercise.course_id, request.course_id, "course_id")
@@ -35,6 +39,8 @@ def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
     :param expected: ожидаемые данные
     :raises AssertionError: если одно поле не совпадает
     """
+    logger.info("Check exercise")
+
     assert_equal(actual.id, expected.id, "id")
     assert_equal(actual.title, expected.title, "title")
     assert_equal(actual.course_id, expected.course_id, "course_id")
@@ -57,6 +63,7 @@ def assert_get_exercise_response(
     :param create_exercise_response: ответ на создание задания
     :raises AssertionError: если фактический ответ не соответствует ожидаемому
     """
+    logger.info("Check get exercise response")
 
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
 
@@ -69,6 +76,7 @@ def assert_update_exercise_response(response: UpdateExerciseResponseSchema, requ
     :param request: ожидаемые данные
     :raises AssertionError: если хоть одно поле не совпадает
     """
+    logger.info("Check update exercise response")
 
     assert_equal(response.exercise.title, request.title, "title")
     assert_equal(response.exercise.max_score, request.max_score, "max_score")
@@ -85,6 +93,7 @@ def assert_exercise_not_found_response(actual: InternalErrorResponseSchema):
     :param actual: фактический ответ
     :raises AssertionError: если фактический ответ не соответствует "Exercise not found"
     """
+    logger.info("Check exercise not found response")
 
     expected = InternalErrorResponseSchema(details="Exercise not found")
     assert_internal_error_response(actual, expected)
@@ -98,6 +107,7 @@ def assert_get_exercises_response(get_exercise_response: GetExercisesResponseSch
     :param get_exercise_response: Ответ API при запросе получения списка заданий
     :param create_exercises_response: Ответ API при создании списка заданий
     """
+    logger.info("Check get exercises response")
 
     assert_length(get_exercise_response.exercises, create_exercises_response, "exercises")
 
